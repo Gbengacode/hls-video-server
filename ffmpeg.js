d3072ca3d1a47849b2cd4afd8708b9d7d4c34ab2
emmanuel <gbengacode>
2023-01-06 09:19:31 +0100

    Add hls server and video streaming

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,15 +1,19 @@
 import Ffmpeg from "fluent-ffmpeg"
 import FfmpegInstaller from "@ffmpeg-installer/ffmpeg"
 
-Ffmpeg.setFfmpegPath(FfmpegInstaller.path)
+const outputVideo = () => {
+    Ffmpeg.setFfmpegPath(FfmpegInstaller.path)
 
-Ffmpeg('videos/video.mp4', { timeout: 432000}).addOptions([
-    '-profile:v baseline',
-    '-level 3.0',
-    '-start_number 0',
-    '-hls_time 10',
-    '-hls_list_size 0',
-    '-f hls'
-]).output('videos/output.m3u8').on('end', () => {
-    console.log('end')
-}).run()
+    Ffmpeg('videos/video.mp4', { timeout: 432000}).addOptions([
+        '-profile:v baseline',
+        '-level 3.0',
+        '-start_number 0',
+        '-hls_time 10',
+        '-hls_list_size 0',
+        '-f hls'
+    ]).output('videos/output.m3u8').on('end', () => {
+        console.log('end')
+    }).run()
+}
+
+export default outputVideo
